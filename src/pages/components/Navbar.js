e56a192c3ec1d92bf3e37aab238cdd34a25055a6
nickleon07 <nick.r.leon@gmail.com>
2021-12-16 16:22:36 -0800

    yet another commit

--- src/pages/components/Navbar.js
+++ src/pages/components/Navbar.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import { Link } from 'gatsby';
 
+const test = {
+  backgroundColor: "white",
+  justifyContent: "space-evenly"
+}
+
 let Nav = () => {
   return (
     <nav>
       <h1>Nick's gatsby site</h1>
-      <div className="links">
+      <div className="links" style={test}>
         <Link to="/">Home </Link>
-
         <Link to="/about">About </Link>
-
         <Link to="/projects/flashcard">Flashcard </Link>
       </div>
     </nav>
--- src/pages/projects/flashcard.js
+++ src/pages/projects/flashcard.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Nav from '../components/Navbar.js'
 
 let aboutFlashcard = () => {
   return (
