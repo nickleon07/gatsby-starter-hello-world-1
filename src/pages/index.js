4478ce38bec687ad0e91b41a30052bfa6099992e
nickleon07 <nick.r.leon@gmail.com>
2021-12-18 14:33:09 -0800

    more screwing around with typist and animations

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,13 @@
 import React from "react"
 import Layout from "./components/Layout.js"
+import Typist from "react-typist";
 
 export default function Home() {
   return (
   <Layout>
+  <Typist avgTypingDelay={20}>
   <div>Hello world!</div>
+  </Typist>
   </Layout>
   );
 };
--- src/pages/projects/flashcard.js
+++ src/pages/projects/flashcard.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import Layout from '../components/Layout.js'
+import Typist from 'react-typist';
 
 let aboutFlashcard = () => {
   return (
     <Layout>
+      <Typist avgTypingDelay={20}>
     <div> This is where I would link some information about my flashcard project</div>
+    </Typist>
     </Layout>
   );
 }
